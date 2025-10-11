7ab8d6209694799066f61b82e433c9868c056be3
ranga <ranga@Ranga>
2025-10-11 20:00:12 +0530

    daynine completed

---
+++ CoreJava/src/com/tnsif/daynine/StringSubclass.java
@@ -0,0 +1,12 @@
+//to demonstrate the error
+
+//can't inherit final classes
+package com.tnsif.daynine;
+
+public class StringSubclass extends String {
+
+	public static void main(String[] args) {
+		
+
+	}
+}s
